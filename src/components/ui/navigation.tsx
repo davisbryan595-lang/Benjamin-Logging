3535e82a4230d8ba57a76465bca16468970d3f44
Wasif <rwasif280@gmail.com>
2025-09-17 02:46:23 +0500

    Update navigation.tsx

--- src/components/ui/navigation.tsx
+++ src/components/ui/navigation.tsx
@@ -26,9 +26,9 @@ const Navigation = () => {
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center">
             <img
-              src="/logo2.png"   // place logo.png in your project's public folder
+              src="/logo.png"   // place logo.png in your project's public folder
               alt="Logo"
-              className="h-8 w-3 object-contain"/>
+              className="h-12 w-auto object-contain"/>
           </div>
 
           
