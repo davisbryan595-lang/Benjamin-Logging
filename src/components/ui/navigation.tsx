8f8c1193a266464bf8b3e6ecd67dcfb3ae8b0e50
Wasif <rwasif280@gmail.com>
2025-09-17 02:21:32 +0500

    Update navigation.tsx

--- src/components/ui/navigation.tsx
+++ src/components/ui/navigation.tsx
@@ -28,7 +28,7 @@ const Navigation = () => {
             <img
               src="/logo.png"   // place logo.png in your project's public folder
               alt="Logo"
-              className="h-13 w-auto object-contain"
+              className="h-14 w-auto object-cover"
             />
           </div>
 
